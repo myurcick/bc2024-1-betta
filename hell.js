aacddb5e455c7e5439e4f0885b8aadbd3daf0691
Мазур Юрій bc2024-1 <yurii.mazur@lnu.edu.ua>
2024-09-05 18:26:52 +0300

    Зробив по нововму працює стабільно ніби

--- hell.js
+++ hell.js
@@ -2,14 +2,9 @@ const { exec } = require('child_process');
 
 // Виконуємо команду для отримання імені користувача
 exec('git config user.name', (error, stdout, stderr) => {
-  // if (error) {
-    //   console.error(`Помилка: ${error.message}`);
-    //  return;
-   // }
-   // if (stderr) {
-       // console.error(`Помилка: ${stderr}`);
-      // return;
+ 
     const githubUsername = stdout.trim();
     console.log(`Hello, ${githubUsername}`);
+console.log(`����� ����������, ${githubUsername}`);
 });
 
